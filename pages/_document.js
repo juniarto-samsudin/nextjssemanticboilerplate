4bbc9c2a1a9e34e60bf0108e07c269ff8cb2d8b5
juniarto <juniarto.samsudin@gmail.com>
2022-08-04 09:05:30 +0800

    include semantic-ui

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"/>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
